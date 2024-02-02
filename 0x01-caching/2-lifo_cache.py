67a32a8bd8d4556ca0b7c45680fad14ae202a971
H-Asmaa <asmaehadar32@gmail.com>
2024-02-02 15:29:07 +0100

    Committing the class LIFOCache where we test caching using the algorithm LIFO.

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+0x01. Caching
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Last In, First Out Cache (FIFO)."""
+
+    lastItemKey = None
+
+    def __init__(self):
+        """The init of the class."""
+        super().__init__()
+
+    def put(self, key, item):
+        """A method that adds an item to the cache"""
+        if item and key:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                self.lastItemKey = key
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                del self.cache_data[self.lastItemKey]
+                print("DISCARD: {}".format(self.lastItemKey))
+            self.cache_data[key] = item
+            self.lastItemKey = key
+
+    def get(self, key):
+        """A method that gets data from cache by key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
