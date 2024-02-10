3e34a0d99bd3d261a91a75c46e5f74f4dc132a1b
H-Asmaa <asmaehadar32@gmail.com>
2024-02-10 13:36:24 +0100

    Committing a correction for the flask app.

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,15 +5,17 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config:
-    """A class Config"""
+class Config(object):
+    """The configuration class for the application."""
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCAL = "en"
-    BABEL_TIMEZONE = "UTC"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
