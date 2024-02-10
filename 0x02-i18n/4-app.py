de4f2056b0ef870e1861b066bcfb94dfa2e46a17
H-Asmaa <asmaehadar32@gmail.com>
2024-02-10 15:52:05 +0100

    Committing an update for get_local configuring language detection from URL requested.

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+0x02. i18n
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """The configuration class for the application."""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def main():
+    """A basic flask method."""
+    return render_template("3-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """Determine which locale to use based on user's request header."""
+    language = request.args['locale']
+    if 'locale' in request.args and language in app.config['LANGUAGES']:
+        return language
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5001")
