8a12cbb788245d713a720527f3e552145177a137
H-Asmaa <asmaehadar32@gmail.com>
2024-03-06 22:59:32 +0100

    Committing a system of two a job creator and a job processor.
    Fixing the jobType.

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -6,10 +6,10 @@ const jobObject = {
   message: 'This is the code to verify your account',
 };
 
-const push_notification_code = kue.createQueue();
+const queue = kue.createQueue();
 
-const jobType = 'push_notification';
-const job = push_notification_code.create(jobType, jobObject).save((error) => {
+const jobType = 'push_notification_code';
+const job = queue.create(jobType, jobObject).save((error) => {
   if (!error) console.log(`Notification job created: ${job.id}`);
   else console.log('Notification job failed');
 });
--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -9,7 +9,7 @@ function sendNotification(phoneNumber, message) {
   );
 }
 
-queue.process('push_notification', (job, done) => {
+queue.process('push_notification_code', (job, done) => {
   const { phoneNumber, message } = job.data;
   sendNotification(phoneNumber, message);
   done();
